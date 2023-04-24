51c47c3ed62f5842d8a033715cec2c118af89ead
kaanc <kaancolak3458@gmail.com>
2023-04-24 14:56:02 +0300

    Updated

---
+++ AddressBookDL/ImplementationsOfRepo/UserAddressRepo.cs
@@ -0,0 +1,18 @@
+﻿using AddressBookDL.InterfacesOfRepo;
+using AddressBookEL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDL.ImplementationsOfRepo
+{
+    public class UserAddressRepo:Repository<UserAddress,int>,IUserAddressRepo
+    {
+        public UserAddressRepo(MyContext c):base(c) //context yerine c de yazılabilir farketmez. Gstermek için yapıldı!
+        {
+            
+        }
+    }
+}
