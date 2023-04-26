38edb4919f2aeb66704508a0ac6e9cae4f89fe45
kaanc <kaancolak3458@gmail.com>
2023-04-26 08:50:02 +0300

    updated

---
+++ AddressBookPL/Models/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookPL.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50,MinimumLength =2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        //Regular Expression yazılmalı
+        public string Email { get; set; }
+        [Required]
+        [StringLength (11, MinimumLength = 11)]
+        //Regular Expression yazılmalı
+        public string Phone { get; set; }
+        public DateTime? BirthDate { get; set; } //? --> null gelebilir
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
--- AddressBookPL/Program.cs
+++ AddressBookPL/Program.cs
@@ -1,6 +1,9 @@
 ﻿using AddressBookDL;
+using AddressBookEL.IdentityModels;
 using AddressBookEL.Mapping;
+using AddressBookPL.DefaultData;
 using AutoMapper.Extensions.ExpressionMapping;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,20 @@ builder.Services.AddDbContext<MyContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
 });
+//identity ayarları
+builder.Services.AddIdentity<AppUser,AppRole>(options =>
+{
+    //ayarlar eklenecek
+    options.Password.RequiredLength = 4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false; //@ / () [] {} ? : ; karakterler
+    options.Password.RequireDigit = false;  
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz-_0123456789";
+
+
+}).AddDefaultTokenProviders().AddEntityFrameworkStores<MyContext>();
 
 //auto mapper ayarları
 
@@ -41,4 +58,28 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+
+//Proje ilk çalışacağı zaman default olarak eklenmesini istediğiniz verileri ya da başka işlemleri yazdığınız class'ı burada çağırmalısınız
+
+//buraya geri döneceğiz
+
+//app.Data(); //extension metot olarak çağırmak
+//DataDefault().Data(app); //harici çağırmak
+
+//Xihan Shen ablanın yönteminden yapalım böylece Erdener'in static olmasın isteğini yerine getirelim!
+
+
+using (var scope = app.Services.CreateScope())
+{
+    //Resolve ASP .NET Core Identity with DI help
+    var userManager = (UserManager<AppUser>?)scope.ServiceProvider.GetService(typeof(UserManager<AppUser>));
+    var roleManager = (RoleManager<AppRole>?)scope.ServiceProvider.GetService(typeof(RoleManager<AppRole>));
+    // do you things here
+
+    DataDefaultXihan d = new DataDefaultXihan();
+    d.CheckAndCreateRoles(roleManager);
+
+}
+
+
+app.Run(); //uygulamayı çalıştırır
